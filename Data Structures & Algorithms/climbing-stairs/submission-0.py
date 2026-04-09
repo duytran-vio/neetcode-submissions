2a35e5b87514974f740a763cb81e8b6420e92bb5
duytran-vio <65882917+duytran-vio@users.noreply.github.com>
2026-04-09 21:41:59 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev, curr = 1, 1
+        
+        for _ in range(n):
+            prev, curr = curr, prev + curr
+            
+        return prev
