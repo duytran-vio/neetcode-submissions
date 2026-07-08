82c6afd56f8f85d4eeb1d9d5a406669fa8b2641b
duytran-vio <65882917+duytran-vio@users.noreply.github.com>
2026-07-08 10:13:53 +0700

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.py
@@ -0,0 +1,24 @@
+
+import string
+class Solution:
+    def decodeString(self, s: str) -> str:
+        st = []
+        st.append([1, ""])
+        i = 0
+        while i < len(s):
+            if s[i] in string.digits:
+                start_repeat = i
+                while i < len(s) and s[i] in string.digits:
+                    i += 1
+                repeat = int(s[start_repeat:i])
+                st.append([repeat, ""])
+            elif s[i] in string.ascii_lowercase:
+                st[-1][1] += s[i]
+            elif s[i] == ']':
+                repeat, substr = st.pop()
+                repeated_substr = ""
+                for j in range(repeat):
+                    repeated_substr += substr
+                st[-1][1] += repeated_substr
+            i += 1
+        return st[-1][1]
